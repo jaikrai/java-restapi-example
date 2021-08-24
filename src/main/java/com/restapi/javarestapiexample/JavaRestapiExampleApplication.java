240b24803a2ac5e4404df667a96d67224f57d82b
jaikrai <jaikrai0@gmail.com>
2021-08-24 16:14:18 -0400

    inital commit

---
+++ src/main/java/com/restapi/javarestapiexample/JavaRestapiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.javarestapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestapiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestapiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/javarestapiexample/JavaRestapiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.javarestapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestapiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
